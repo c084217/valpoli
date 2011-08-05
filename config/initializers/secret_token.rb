7c129c603f2360bda871c29f50b7e654a89284e2
shiroshana <shiroshana@gmail.com>
2011-08-05 07:41:06 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Valpoli::Application.config.secret_token = '24a96730e8e73c623806917bd92755b4a9ae0f6ade09a12da1ccc74943a045c41ef7eeb02ba43ba2feef6569998f54d4c53558d430827d5699e40d1ef66487a8'
